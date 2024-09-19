8f2815dcd2a85b12b13ff396212620bf52b7b155
Pravakar Rijal <83540242+Pravakar-RijaI@users.noreply.github.com>
2024-09-19 15:58:33 +0545

    Update StringDemo.java

--- class_01/StringDemo.java
+++ class_01/StringDemo.java
@@ -3,7 +3,7 @@ class StringDemo
 	public static void main(String[] args)
 	{
 		Demo demo = new Demo();
-		String name = demo.display(args[0]);
-		System.out.pritnln(name);
+		String name = args[0];
+		System.out.pritnln(demo.display(name));
 	}
 }
